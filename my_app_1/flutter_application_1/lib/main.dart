0370609479b509f789fd9d527ce74fb3ef6d47bb
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-04-09 13:59:59 +0200

    session 30

--- my_app_1/flutter_application_1/lib/main.dart
+++ my_app_1/flutter_application_1/lib/main.dart
@@ -8,14 +8,36 @@ void main() => runApp(MyApp()); // shortcut to make code cleaner
 
 class MyApp extends StatelessWidget {
   @override // Override the default build method, we ovverride the build method to return a widget of our own
+  // ignore: dead_code, dead_code
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+      "What's your favorite food?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("My First App"),
         ),
-        body: const Text("Hello World"),
+        body: Column(
+          children: const [
+            Text("The question!"),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 1"), // content inside the button
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 2"), // content inside the button
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 3"), // content inside the button
+            ),
+          ],
+        ),
       ),
-    );
+    ); // body only admits 1 widget
   }
 }
