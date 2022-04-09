efb1a9df8b99ded355597c93f9fbbec417dba26a
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-04-09 13:45:26 +0200

    session 28

--- my_app_1/flutter_application_1/lib/main.dart
+++ my_app_1/flutter_application_1/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp()); // shortcut to make code cleaner
 
 class MyApp extends StatelessWidget {
+  @override // Override the default build method, we ovverride the build method to return a widget of our own
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My First App"),
+        ),
+        body: const Text("Hello World"),
+      ),
     );
   }
 }
