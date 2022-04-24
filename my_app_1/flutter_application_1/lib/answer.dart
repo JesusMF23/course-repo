27051352269187f6443cdd76378e47db1d2dd127
JesusMF23 <jesusmaldonadofernandez@gmail.com>
2022-04-24 20:06:41 +0200

    session-51-output conditionally

--- my_app_1/flutter_application_1/lib/answer.dart
+++ my_app_1/flutter_application_1/lib/answer.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
   final String answerText;
+  //final if a value doesn't change when the program launches, runtime constant
 
   Answer(this.selectHandler, this.answerText);
 
@@ -10,6 +11,9 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: const EdgeInsets.all(2),
+      //const is a compile time constant
+      padding: const EdgeInsets.all(2),
       child: ElevatedButton(
         style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
--- my_app_1/flutter_application_1/lib/main.dart
+++ my_app_1/flutter_application_1/lib/main.dart
@@ -23,7 +23,20 @@ class _MyAppState extends State<MyApp> {
   // this second class is the state and is not rebuilt every time state changes
   //alternative syntax
   // class _MyAppState -> the _ is a private class and is not accessible outside of this file
-
+  final questions = [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["Red", "Green", "Blue"]
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Dog", "Cat", "Bird"]
+    },
+    {
+      "questionText": "What's your favorite food?",
+      "answers": ["Pizza", "Sushi", "Pasta", "Burger"]
+    }
+  ];
   void _answerQuestion() {
     setState(() {
       //setstate forces flutter to re-render, calls build method again of the widget class where is contained
@@ -31,6 +44,11 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     }); //we need to add a set state to the function that generates the event and include the logic for changing the state
     //we increase the index with each answer by 1
+    if (_questionIndex < questions.length) {
+      print("We have more questions!");
+    } else {
+      print("No more questions!");
+    }
     print('Answer chosen!');
   }
 
@@ -39,38 +57,27 @@ class _MyAppState extends State<MyApp> {
   @override // Override the default build method, we ovverride the build method to return a widget of our own
   // ignore: dead_code, dead_code
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answers": ["Red", "Green", "Blue"]
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answers": ["Dog", "Cat", "Bird"]
-      },
-      {
-        "questionText": "What's your favorite food?",
-        "answers": ["Pizza", "Sushi", "Pasta", "Burger"]
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("My First App"),
+          title: const Text("Quiz App"),
         ),
-        body: Column(
-          children: [
-            Question(//we are using the custom widget built in question.dart
-                questions[_questionIndex]["questionText"]
-                    as String), //could use questions.elementAt(0)
-            ...(questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              // the ... takes all values in a list and get them out of the list to the parent widget
-              // in this case, we add the values of a list to the parent widget list
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                      //we are using the custom widget built in question.dart
+                      questions[_questionIndex]["questionText"]
+                          as String), //could use questions.elementAt(0)
+                  ...(questions[_questionIndex]["answers"] as List<String>)
+                      .map((answer) {
+                    // the ... takes all values in a list and get them out of the list to the parent widget
+                    // in this case, we add the values of a list to the parent widget list
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(child: Text("You reached the end!")),
       ),
     ); // body only admits 1 widget
   }
